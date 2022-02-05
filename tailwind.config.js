dfc47b4bad85f9e8569ce68a3c8d3943df870c3e
Jakhongir Kayumov <jakhongir.dev@gmail.com>
2022-02-05 17:40:17 +0500

    added custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,30 @@
+const { color } = require("@mui/system");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      blue: colors.blue,
+      slate: colors.slate,
+      red: colors.red,
+      pink: colors.pink,
+      rose: colors.rose,
+      orange: colors.orange,
+      lime: colors.lime,
+      green: colors.green,
+      "blue-dark": "#303654",
+      "blue-dark-2": "#232842",
+    },
   },
   plugins: [],
 };
