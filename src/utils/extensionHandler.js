246ef6b1cb6660b8971e22ba92858a29dd681574
Jakhongir Kayumov <jakhongir.dev@gmail.com>
2022-02-11 16:00:54 +0500

    fix ext detect logic

--- src/utils/extensionHandler.js
+++ src/utils/extensionHandler.js
@@ -1,10 +1,11 @@
 const imgExts = ["jpg", "png", "jpeg", "gif"];
 const docExts = ["doc", "docx", "pdf", "txt", "ppt", "xls", "xlsx"];
 const videoExts = ["mp4", "mov", "avi", "webm"];
-const musicExts = ["jpg", "png", "jpeg", "gif"];
+const musicExts = ["mp3", "wma"];
 
 export default function extensionHandler(name) {
-  const ext = name.split(".")[1];
+  const charsArr = name.split(".");
+  const ext = charsArr[charsArr.length - 1];
   if (imgExts.includes(ext)) {
     return "img";
   } else if (docExts.includes(ext)) {
