739446da730086daec55589a0ea2ab60084f7846
Jakhongir Kayumov <jakhongir.dev@gmail.com>
2022-02-11 15:00:10 +0500

    Add redux store

---
+++ src/redux/store.js
@@ -0,0 +1,79 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const uploadSlice = createSlice({
+  name: "upload",
+  initialState: { didUpload: false },
+  reducers: {
+    upload(state) {
+      state.didUpload = !state.didUpload;
+    },
+  },
+});
+
+const allFilesSlice = createSlice({
+  name: "files",
+  initialState: { files: [] },
+  reducers: {
+    add(state, action) {
+      state.files = action.payload;
+    },
+  },
+});
+
+const imagesSlice = createSlice({
+  name: "images",
+  initialState: { images: [] },
+  reducers: {
+    add(state, action) {
+      state.images = action.payload;
+    },
+  },
+});
+
+const docsSlice = createSlice({
+  name: "docs",
+  initialState: { docs: [] },
+  reducers: {
+    add(state, action) {
+      state.docs = action.payload;
+    },
+  },
+});
+const videosSlice = createSlice({
+  name: "videos",
+  initialState: { videos: [] },
+  reducers: {
+    add(state, action) {
+      state.videos = action.payload;
+    },
+  },
+});
+const musicSlice = createSlice({
+  name: "music",
+  initialState: { music: [] },
+  reducers: {
+    add(state, action) {
+      state.music = action.payload;
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    upload: uploadSlice.reducer,
+    allFiles: allFilesSlice.reducer,
+    images: imagesSlice.reducer,
+    docs: docsSlice.reducer,
+    videos: videosSlice.reducer,
+    music: musicSlice.reducer,
+  },
+});
+
+export const uploadActions = uploadSlice.actions;
+export const allFilesActions = allFilesSlice.actions;
+export const imagesActions = imagesSlice.actions;
+export const docsActions = docsSlice.actions;
+export const videosActions = videosSlice.actions;
+export const musicActions = musicSlice.actions;
+
+export default store;
