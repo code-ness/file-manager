953772520b10e974a1c764c9b2195246d36cd684
Jakhongir Kayumov <jakhongir.dev@gmail.com>
2022-02-12 17:02:05 +0500

    fix sidebar style by making it stick to bottom and fixed with grid

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import SideBar from "../layout/SideBar/SideBar";
-import Dashboard from "../pages/dashboard/Dashboard";
+import { Outlet } from "react-router-dom";
+import SideBar from "../layout/SideBar";
 
 export default function MainContainer() {
   return (
-    <div className="flex w-full">
-      <header className="bg-blue-dark">
+    <div className="w-full grid grid-cols-[80px_1fr]">
+      <header className="bg-blue-dark fixed top-0 bottom-0">
         <SideBar />
       </header>
-      <main className="w-full">
+      <main className="w-full col-start-2 col-end-3">
         <div className="px-12 py-8 bg-blue-dark border-l border-blue-dark-2">
           <h1 className="text-5xl font-bold text-slate-400">File Manager</h1>
         </div>
-        <Dashboard />
+        <Outlet />
       </main>
     </div>
   );
