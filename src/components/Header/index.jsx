d9e5cc0c6fd41c0f737ae6d411012d1c7a5613ad
Jakhongir Kayumov <jakhongir.dev@gmail.com>
2022-02-16 13:06:53 +0500

    add modal for DragAndDrop component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import UploadButton from "../../components/UploadButton";
+import DragAndDrop from "../DragAndDrop";
 
 export default function Header({ title }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="flex justify-between items-center">
       <h2 className="text-5xl font-bold text-blue-dark">{title}</h2>
-      <UploadButton />
+      <UploadButton onAddModal={() => setIsOpen(true)} />
+      {isOpen && <DragAndDrop onClose={setIsOpen(false)} />}
     </div>
   );
 }
