ebc2fa19dcdc71774eee2a5f6bb6e0a81b6ea3a7
Jakhongir Kayumov <jakhongir.dev@gmail.com>
2022-02-16 13:10:34 +0500

    make progress bar dynamic and add animation

--- src/components/StorageItem/index.jsx
+++ src/components/StorageItem/index.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { LinearProgress } from "@mui/material";
 
-export default function StorageItem({ title, icon }) {
+export default function StorageItem({ title, icon, spaceUsed, spaceLimit }) {
+  const progress = (spaceUsed / spaceLimit) * 100;
   return (
-    <div className="p-16 px-24 bg-slate-200 rounded-3xl">
+    <div className="p-16 px-24 bg-slate-200 rounded-3xl shadow-xl">
       <div className="mb-16 flex items-center">
         {icon}
         <p className="ml-8 mr-32 text-3xl font-bold">{title}</p>
       </div>
       <p className="mb-4 text-2xl text-slate-800 font-bold">
-        15 of 100 images used
+        {`${spaceUsed} MB of ${spaceLimit} MB used`}
       </p>
       <LinearProgress
         variant="determinate"
-        value={15}
+        value={progress}
         sx={{ height: "1rem", borderRadius: "1rem" }}
       />
     </div>
