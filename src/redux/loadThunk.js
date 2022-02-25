43ff1aabc2f4f5c5680ce101598be784ce9f1499
Jakhongir Kayumov <jakhongir.dev@gmail.com>
2022-02-25 15:51:28 +0500

    delete unused statements

--- src/redux/loadThunk.js
+++ src/redux/loadThunk.js
@@ -1,10 +1,4 @@
-import {
-  imagesActions,
-  docsActions,
-  videosActions,
-  musicActions,
-  allFilesActions,
-} from "./store";
+import { allFilesActions } from "./store";
 import { ref, listAll, getMetadata } from "firebase/storage";
 import { storage } from "../firebase";
 import sortByDate from "../utils/sortByDate";
@@ -46,63 +40,6 @@ export function loadAllFiles() {
       dispatch(allFilesActions.addDocs(docs));
       dispatch(allFilesActions.addVideos(videos));
       dispatch(allFilesActions.addMusic(music));
-    } catch (err) {
-      console.error(err);
-    }
-  };
-}
-export function loadImages() {
-  return async function (dispatch) {
-    try {
-      const listRef = ref(storage, "images");
-      const res = await listAll(listRef);
-      const images = await Promise.all(
-        res.items.map((item) => getMetadata(item))
-      );
-      dispatch(imagesActions.add(images));
-    } catch (err) {
-      console.error(err);
-    }
-  };
-}
-
-export function loadDocs() {
-  return async function (dispatch) {
-    try {
-      const listRef = ref(storage, "docs");
-      const res = await listAll(listRef);
-      const docs = await Promise.all(
-        res.items.map((item) => getMetadata(item))
-      );
-      dispatch(docsActions.add(docs));
-    } catch (err) {
-      console.error(err);
-    }
-  };
-}
-export function loadVideos() {
-  return async function (dispatch) {
-    try {
-      const listRef = ref(storage, "videos");
-      const res = await listAll(listRef);
-      const videos = await Promise.all(
-        res.items.map((item) => getMetadata(item))
-      );
-      dispatch(videosActions.add(videos));
-    } catch (err) {
-      console.error(err);
-    }
-  };
-}
-export function loadMusic() {
-  return async function (dispatch) {
-    try {
-      const listRef = ref(storage, "music");
-      const res = await listAll(listRef);
-      const music = await Promise.all(
-        res.items.map((item) => getMetadata(item))
-      );
-      dispatch(musicActions.add(music));
     } catch (err) {
       console.error(err);
     }
